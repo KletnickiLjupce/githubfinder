4dfb0e62e189e8a44e096e682cc68d889c1735b3
KletnickiLjupce <KletnickiLjupce@users.noreply.github.com>
2020-09-17 01:30:32 +0200

    Initial commit

---
+++ src/components/pages/NotFund.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const NotFund = () => {
+  return (
+    <div>
+      <h1>Not found</h1>
+      <p className="lead">The page you are looking for is does not exist.</p>
+    </div>
+  );
+};
+
+export default NotFund;
